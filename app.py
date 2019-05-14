614316e9ae907a261126b0130cd2e2d22c7bbb4c
Christian Tietze <christian.tietze@gmail.com>
2019-05-14 16:05:48 +0100

    implement shutdown

--- app.py
+++ app.py
@@ -152,20 +152,26 @@ class App:
         self.toggle_blink(False)
         self.current_menu = None
 
-    # Menu action
+    # Menu action (delegate calls)
+    
     def on_next_chapter(self):
         print "Exec Next Chapter"
+
     def on_prev_chapter(self):
         print "Exec Prev Chapter"
+
     def on_next_book(self):
         print "Exec Next Book"
+
     def on_prev_book(self):
         print "Exec Prev Book"
+
     def on_shutdown(self):
         print "Exec Shutdown"
-        # check_call(['sudo', 'poweroff'])
+        check_call ['sudo', 'poweroff']
+
     def on_continue(self):
-        print "Exec continue"
+        print "Exec continue (closing menu)"
         self.close_menu()
 
 ##############################################################################
